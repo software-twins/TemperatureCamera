7013160766db576e83be4342e49584f15abef69b
Sergej Alexejew <sergej@localhost.localdomain>
2021-04-27 15:23:39 +0300

    27.04.21&2?

---
+++ Assets/Orbit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Orbit : MonoBehaviour
+	{
+    	public Camera cam;
+
+		// Start is called before the first frame update
+    	void Start ()
+    		{
+				cam.transform.LookAt (targetObject, Vector3.up);
+			}
+     	
+    	public Transform targetObject;
+		public float 	 orbitSpeed = 10.0f, moveSpeed = 3.0f;
+
+		private float scroll, delta;
+
+    	// Update is called once per frame
+    	void Update ()
+    		{
+				if ( Input.GetMouseButton (0) ) 
+					{
+    					delta = Input.GetAxis ("Mouse X");
+        				cam.transform.RotateAround (targetObject.transform.position, Vector3.up, delta * orbitSpeed);
+    				}
+		 
+				scroll = Input.GetAxis ("Vertical") * .01f; //; //("Mouse ScrollWheel");
+           		cam.transform.Translate (0, 0, scroll, Space.Self);
+
+				if ( Input.GetMouseButton (2) ) 
+					{
+						delta = Input.GetAxis ("Mouse X");
+        				cam.transform.Translate (0.01f * delta * moveSpeed, 0, 0, Space.Self);
+    				}
+    		}
+	}
---
+++ Assets/TemperatureCanvas.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TemperatureCanvas : MonoBehaviour
+	{
+		public Camera camera;
+
+		private Canvas canvas;
+
+ 		void Start	()
+    		{
+        		// Get canvas from the GameObject.
+        Canvas canvas;
+        canvas = canvasGO.GetComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+
+        // Create the Text GameObject.
+        GameObject textGO = new GameObject();
+        textGO.transform.parent = canvasGO.transform;
+        textGO.AddComponent<Text>();
+
+        // Set Text component properties.
+        text = textGO.GetComponent<Text>();
+        text.font = arial;
+        text.text = "Press space key";
+        text.fontSize = 48;
+        text.alignment = TextAnchor.MiddleCenter;
+
+        // Provide Text position and size using RectTransform.
+        RectTransform rectTransform;
+        rectTransform = text.GetComponent<RectTransform>();
+        rectTransform.localPosition = new Vector3(0, 0, 0);
+        rectTransform.sizeDelta = new Vector2(600, 200);
+    }
+
+    void Update()
+    {
+        // Press the space key to change the Text message.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (textChanged != UpDown.Down)
+            {
+                text.text = "Text changed";
+                textChanged = UpDown.Down;
+            }
+            else
+            {
+                text.text = "Text changed back";
+                textChanged = UpDown.Up;
+            }
+        }
+    }
+}
+	}
+
