e86350010a87cb6b8975740d8522f2754ed67c3a
Sergej Alexejew <sergej@localhost.localdomain>
2021-04-28 17:22:12 +0300

    27.04.21&2?

--- Assets/Orbit.cs
+++ Assets/Orbit.cs
@@ -34,5 +34,45 @@ public class Orbit : MonoBehaviour
 						float delta = Input.GetAxis ("Mouse X");
         				cam.transform.Translate (0.01f * delta * moveSpeed, 0, 0, Space.Self);
     				}
+
+			//	FixedUpdate1 ();
     		}
+
+		void FixedUpdate1 ()
+    		{
+        		// Bit shift the index of the layer (8) to get a bit mask
+//        		int layerMask = 1 << 8;
+
+        		// This would cast rays only against colliders in layer 8.
+        		// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+  //      		layerMask = ~layerMask;
+
+				//RectTransform r = GetComponent <RectTransform> ();
+
+  				Vector3 center = cam.transform.position; //ScreenToWorldPoint (new Vector3 (r.rect.width / 2, 
+												//					  r.rect.height / 2,
+												//		 			  cam.nearClipPlane));
+
+				//Debug.Log ("center " + center + " " + r.rect.width + " " + r.rect.height);
+				Debug.DrawRay (center, transform.TransformDirection(Vector3.forward) * 1000, 
+									   Color.green);
+
+        		RaycastHit hit;
+        		// Does the ray intersect any objects excluding the player layer
+        		if (Physics.Raycast (center, transform.TransformDirection(Vector3.forward), 
+									 out hit))
+        			{
+            			Debug.DrawRay (center, 
+									   transform.TransformDirection(Vector3.forward) * hit.distance, 
+									   Color.yellow);
+						var obj = hit.transform.gameObject;
+						Debug.Log("Did Hit" + hit.distance + " " + obj);
+        			}
+        		else
+        			{
+            			Debug.DrawRay (center, 
+									   transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            			Debug.Log("Did not Hit");
+        			}
+			}
 	}
--- Assets/TemperatureCanvas.cs
+++ Assets/TemperatureCanvas.cs
@@ -31,20 +31,66 @@ public class TemperatureCanvas : MonoBehaviour
 
 				text.color = Color.black;
         		text.font = arial;
-        		text.text = "Press space key";
-        		text.fontSize = 28;
-        		text.alignment = TextAnchor.MiddleCenter;
+        		//text.text = "Press space key";
+        		text.fontSize = 14;
+        		//text.alignment = TextAnchor.MiddleCenter;
 
         		// Provide Text position and size using RectTransform.
         		RectTransform rectTransform;
         		rectTransform = text.GetComponent <RectTransform> ();
-        		rectTransform.localPosition = new Vector3 (00, 00, 0);
-        		//rectTransform.sizeDelta = new Vector2 (150, 80);
+        		rectTransform.localPosition = new Vector3 (-313, 182, 0);
+        		rectTransform.sizeDelta = new Vector2 (427, 164);
     		}
 
-    	void Update()
+    
+		void FixedUpdate ()
     		{
-				text.text = "Press space key";
+				// Bit shift the index of the layer (8) to get a bit mask
+//        		int layerMask = 1 << 8;
+
+        		// This would cast rays only against colliders in layer 8.
+        		// But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+  //      		layerMask = ~layerMask;
+
+				RectTransform r = GetComponent <RectTransform> ();
+
+  				Vector3 center = cam.transform.position; //ScreenToWorldPoint (new Vector3 (r.rect.width / 2, 
+												//					  r.rect.height / 2,
+												//		 			  cam.nearClipPlane));
+
+				//Debug.Log ("center " + center + " " + r.rect.width + " " + r.rect.height);
+				//Debug.DrawRay (center, transform.TransformDirection(Vector3.forward) * 1000, 
+				//					   Color.green);
+
+				var obj = new object ();
+
+        		RaycastHit hit;
+        		// Does the ray intersect any objects excluding the player layer
+        		if (Physics.Raycast (center, transform.TransformDirection(Vector3.forward), 
+									 out hit))
+        			{
+            			//Debug.DrawRay (center, 
+						//			   transform.TransformDirection(Vector3.forward) * hit.distance, 
+						//			   Color.yellow);
+						obj = hit.transform.gameObject;
+        			}
+        		else
+        			{
+            			//Debug.DrawRay (center, 
+						//			   transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+
+        			}
+			
+				text.text = "object: " + obj + 
+							"\n" +
+							"distance: " + hit.distance +
+							"\n" +
+							"center: " + center;	
+			}
+
+		void Update()
+    		{
+			//	text.text = "Press space key";
        
     		}
 	}
